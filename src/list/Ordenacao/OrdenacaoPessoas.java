71842ea354b508f3ae753aa081b1da7fac385971
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-22 21:56:08 -0300

    feat: update pastas

--- src/list/Ordenacao/OrdenacaoPessoas.java
+++ src/list/Ordenacao/OrdenacaoPessoas.java
@@ -1,6 +1,8 @@
 package list.Ordenacao;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class OrdenacaoPessoas {
@@ -12,6 +14,18 @@ public class OrdenacaoPessoas {
 
     public void adicionarPessoa(String nome, int idade, double altura){
         pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade(){
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new Pessoa.ComparatorPorAltura());
+        return pessoasPorAltura;
     }
 
 
