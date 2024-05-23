b3ac4da615ae22aee3cd499a4e8a1b6f4b4f6f10
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-22 21:45:17 -0300

    feat: create pessoa

---
+++ src/list/Ordenacao/OrdenacaoPessoas.java
@@ -0,0 +1,18 @@
+package list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoas() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+
+}
