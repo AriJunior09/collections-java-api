8805f42be0452c7d7e07b635b205c4f059cdab05
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-22 21:27:58 -0300

    feat: iniciando testes

--- src/list/Pesquisa/CatalogoLivros.java
+++ src/list/Pesquisa/CatalogoLivros.java
@@ -52,4 +52,12 @@ public class CatalogoLivros {
         }
         return livroPorTitulo;
     }
+
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2020);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2022);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 2024);
+    }
 }
