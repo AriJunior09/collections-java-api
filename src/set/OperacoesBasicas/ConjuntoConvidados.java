7578a3ca206784e5d70ef7936654f5448ebd8f62
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-24 20:16:51 -0300

    feat: implementando convidados

---
+++ src/set/OperacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,37 @@
+package set.OperacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoParaRemover = null;
+        for(Convidado c : convidadoSet){
+            if(c.getCodigoConvite() == codigoConvite){
+                convidadoParaRemover = c;
+                break;
+            }else{
+                System.out.println("Não existe convidados com esse código!");
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados(){
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println("Lista de Convidados: \n" + convidadoSet);
+    }
+}
