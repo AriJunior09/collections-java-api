ce0ad47017499e22d7ee30d3244875d1314e98ad
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-24 21:33:43 -0300

    feat: update cadastroProduto

---
+++ src/set/Ordenacao/CadastroProduto.java
@@ -0,0 +1,16 @@
+package set.Ordenacao;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CadastroProduto {
+    private Set<Produto> produtoSet;
+
+    public CadastroProduto() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, double preco, int quantidade){
+        produtoSet.add(new Produto(cod, nome, preco, quantidade));
+    }
+}
