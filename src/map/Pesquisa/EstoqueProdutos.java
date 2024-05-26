727b42cdab310663f848a9efecc07a0dc22223bb
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-05-26 19:15:02 -0300

    feat: update estoqueProdutos

--- src/map/Pesquisa/EstoqueProdutos.java
+++ src/map/Pesquisa/EstoqueProdutos.java
@@ -1,4 +1,5 @@
 package map.Pesquisa;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,11 +10,27 @@ public class EstoqueProdutos {
         this.estoqueProdutosMap = new HashMap<>();
     }
 
-    public void adicionarProduto(Long cod, String nome, Double preco, int quantidade){
+    public void adicionarProduto(Long cod, String nome, Double preco, int quantidade) {
         estoqueProdutosMap.put(cod, new Produto(nome, preco, quantidade);
     }
 
-    public void exibirProduto(){
+    public void exibirProduto() {
         System.out.println(estoqueProdutosMap);
+    }
+
+    public Double calcularValorTotalEstoque() {
+        double ValorTotalEstoque = 0d;
+        if (!estoqueProdutosMap.isEmpty()) {
+            for(Produto p : estoqueProdutosMap.values()) {
+                ValorTotalEstoque += p.getPreco() * p.getQuantidade();
+            }
+        }
+        return ValorTotalEstoque;
+    }
+
+    public Produto obterProdutoMaisCaro(){
+        Produto produtoMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        
     }
 }
